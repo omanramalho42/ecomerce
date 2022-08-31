cc622ce005413d952aa483f2eec34fd5a34bb7e4
omanramalho <omanapple42@hotmail.com>
2022-08-30 23:02:12 -0300

    fix:implementando conexao com banco de dados

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
+mongoose.connect(
+  process.env.MONGO_URL
+)
+.then(() => 
+  console.log('DB connection successfull'))
+.catch((err) => {
+  console.log('Error: ',err)
+});
+
+app.listen(process.env.PORT || 3001, () => {
   console.log("Backend server is running on port 3000")
 });
