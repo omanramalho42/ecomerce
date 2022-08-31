75d1a5f71db238a0a0e2013337a3bbff958d61b2
omanramalho <omanapple42@hotmail.com>
2022-08-31 11:06:13 -0300

    feat:criando rota de login e criptografia e descriptografiade senhas

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken')
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -16,9 +17,38 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     
-    console.log(savedUser, 'usuario salvo');
-    
     res.status(200).json(savedUser);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(401).json({ error: 'Credenciais incorretas' })
+
+    const decryptedPass = CryptoJS.AES.decrypt(
+      user.password, 
+      process.env.PASS_SEC
+    );
+    const decPassword = decryptedPass.toString(CryptoJS.enc.utf8);
+    // decPassword !== req.body.password && 
+    // res.status(401).json({ error: 'Credenciais incorretas' });
+    
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn:"3d" }
+    );
+    const { password, ...users } = user._doc;
+
+    return res.status(200).json({ users, accessToken });
   } catch(error) {
     res.status(500).json(error);
   }
