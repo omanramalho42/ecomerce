514bb5ba682717942e3d0b45a8e3484aff878794
omanramalho <omanapple42@hotmail.com>
2022-08-31 18:50:04 -0300

    fix:fazendo verificacao de token jwt

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,71 @@
-const User = require('../models/User');
 const { verifyTokenAndAuthorization } = require('./verifyToken');
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 const router = require('express').Router();
+//GET
+router.get("/find/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    
+    if(!user) res.status(401).json('Este usuário não existe');
 
+    const { password, ...users } = user._doc;
 
-router.put('/:id', verifyTokenAndAuthorization, (req, res) => {
+    res.status(200).json(users);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ALL
+router.get("/findAllUsers", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const query = req.query.new
+    
+    const users = query 
+      ? await User.find().sort({_id:-1}).limit(5) 
+      : await User.find();
+
+    if(users.length === 0) res.status(401).json('Não existe usuários cadastrados');
+
+    res.status(200).json(users);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+//UPDATE
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
   if(req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
     ).toString();
-  }
+  };
 
   try {
-    const updateUser = User.findByIdAndUpdate(req.params.id, {
-      $set: req.body
-    }, { new: true });
-
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id, 
+      {
+        $set: req.body
+      }, 
+      { new: true });
+  
     res.status(200).json(updateUser);
-  } catch(err) {
-    res.status(500).json(err);
+  } catch(error) {
+    res.status(500).json(error);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const userDeleted = await User.findByIdAndDelete(req.params.id);
+    
+    res.status(200).json(userDeleted);
+  } catch(error) {
+    res.status(500).json(error);
   }
 });
 
